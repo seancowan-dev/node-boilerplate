286956af27061b17fc8c8ca58ba101cce8d4bf36
Sean <sean.cowan1@gmail.com>
2020-04-23 12:14:49 -0400

    updated integration test for /api/*

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,9 +1,9 @@
 const app = require('../src/app')
 
 describe('App', () => {
-  it('GET / responds with 200 containing "Hello, world!"', () => {
+  it('GET /api/* responds 200 with JSON object containing ok: true', () => {
     return supertest(app)
-      .get('/')
-      .expect(200, 'Hello, world!')
+      .get('/api/*')
+      .expect(200, { ok: true })
   })
 })
