757a96e0782e4f68f3d9a8f72bcb77b69063f030
Sean <sean.cowan1@gmail.com>
2020-04-23 12:38:22 -0400

    prepared to do an initial heroku deploy

--- src/app.js
+++ src/app.js
@@ -11,7 +11,8 @@ const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
 
-app.use(morgan(morganOption));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(helmet());
 app.use(cors());
 
