71bb48ca4cd7fa952e17cb3aea0194884c0e5fff
Sean <sean.cowan1@gmail.com>
2020-04-27 18:10:33 -0400

    updated timestamp_trig

--- migrations/006.do.create_timestamp_trig.sql
+++ migrations/006.do.create_timestamp_trig.sql
@@ -2,8 +2,3 @@ CREATE TRIGGER set_timestamp
 BEFORE UPDATE ON registered_users
 FOR EACH ROW
 EXECUTE PROCEDURE trigger_set_timestamp();
-
-CREATE TRIGGER set_timestamp
-BEFORE UPDATE ON list_items
-FOR EACH ROW
-EXECUTE PROCEDURE trigger_set_timestamp();
--- migrations/006.undo.create_timestamp_trig.sql
+++ migrations/006.undo.create_timestamp_trig.sql
@@ -1,5 +1,2 @@
 DROP TRIGGER IF EXISTS set_timestamp
 ON registered_users;
-
-DROP TRIGGER IF EXISTS set_timestamp
-ON list_items;
