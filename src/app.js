004701c902d1632bd810587680680a9db030e1cc
Sean <sean.cowan1@gmail.com>
2020-05-19 17:19:58 -0400

    configuring cors

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ const morganOption = (NODE_ENV === 'production')
 const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
 app.use(morgan(morganSetting))
 app.use(helmet());
-app.options('*', cors());
+// app.options('*', cors());
 app.use(cors({
     origin: CLIENT_ORIGIN
 }));
